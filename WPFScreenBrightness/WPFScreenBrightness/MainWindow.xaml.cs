14cfd63004826a9a784edb960b6fbe09f9451d36
mohammadkk <emperormobiles@yahoo.com>
2018-01-06 09:06:58 +0330

    Alfa

---
+++ WPFScreenBrightness/WPFScreenBrightness/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows;
+using System.Management;
+
+namespace WPFScreenBrightness
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+            Int32 change = Convert.ToInt32(slider.Value);
+            change = change * 6 + 7;
+            byte a = Convert.ToByte(change);
+            SetBrightness(a);
+        }
+
+
+        static void SetBrightness(byte targetBrightness)
+        {
+            ManagementScope scope = new ManagementScope("root\\WMI");
+            SelectQuery query = new SelectQuery("WmiMonitorBrightnessMethods");
+            using (ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, query))
+            {
+                using (ManagementObjectCollection objectCollection = searcher.Get())
+                {
+                    foreach (ManagementObject mObj in objectCollection)
+                    {
+                        mObj.InvokeMethod("WmiSetBrightness",
+                            new Object[] { UInt32.MaxValue, targetBrightness });
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
