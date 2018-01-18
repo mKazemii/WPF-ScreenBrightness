2a9d2d0e0b3a15448a6c262279d374b9da46ce49
mohammadkk <emperormobiles@yahoo.com>
2018-01-18 15:00:26 +0330

    Try Catch

--- WPFScreenBrightness/WPFScreenBrightness/MainWindow.xaml.cs
+++ WPFScreenBrightness/WPFScreenBrightness/MainWindow.xaml.cs
@@ -13,14 +13,27 @@ namespace WPFScreenBrightness
         {
             InitializeComponent();
         }
-
+        bool mainWindowLoad = false;
         private void slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-
+            bool err = false;
             Int32 change = Convert.ToInt32(slider.Value);
             change = change * 6 + 7;
             byte a = Convert.ToByte(change);
-            SetBrightness(a);
+            try
+            {
+                SetBrightness(a);
+            }
+            catch (Exception)
+            {
+                err = true;
+            }
+            if (err && mainWindowLoad)
+            {
+                MessageBox.Show("ارتباط با سیستم نور کامپیوتر شما برقرار نشد\nبرای رفع مشکل کامپیوتر خود را ریستارت کنید", "خطا", MessageBoxButton.OK,MessageBoxImage.Error);
+
+            }
+
         }
 
 
@@ -40,6 +53,11 @@ namespace WPFScreenBrightness
                     }
                 }
             }
+        }
+
+        private void MainWindow1_Loaded(object sender, RoutedEventArgs e)
+        {
+            mainWindowLoad = true;
         }
     }
 }
